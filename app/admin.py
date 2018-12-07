9de71d760eb3a65d74bd8599ebe07a7399dc7cba
ken <zbcmb@outlook.com>
2018-12-07 22:23:25 +0800

    before edit.

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Student)
+admin.site.register(Students)
 
 admin.site.register(CourseType)
-admin.site.register(Course)
-admin.site.register(Department)
-admin.site.register(Teacher)
+admin.site.register(Courses)
+admin.site.register(Departments)
+admin.site.register(Teachers)
--- app/models.py
+++ app/models.py
@@ -3,53 +3,85 @@ from django.db import models
 # Create your models here.
 # 原型类<<<-----------------------------------------------------------------
 
-class Teacher(models.Model):
+class Teachers(models.Model):
 
-    tid = models.IntegerField(unique=True)
+    tid = models.IntegerField(unique=True, primary_key=True)
     name = models.CharField(max_length=30)
     department = models.CharField(max_length=50)
+    # created_date = models.DateTimeField()
 
-class Department(models.Model):
 
-    name = models.CharField(max_length=20,unique=True)
+class Departments(models.Model):
+
+    name = models.CharField(max_length=20,unique=True,primary_key=True)
+    # created_date = models.DateTimeField()
+
 
 # 创建学生类
-class Student(models.Model):
+class Students(models.Model):
 
-    sid = models.IntegerField(unique=True)
+    sid = models.IntegerField(unique=True,primary_key=True)
     name = models.CharField(max_length=20)
     email = models.EmailField()
     # department
-    department = models.ForeignKey(Department,on_delete=models.CASCADE)
+    department = models.ForeignKey(Departments,on_delete=models.CASCADE)
     # password
     password  = models.CharField(max_length=30)
+    # created_date = models.DateTimeField()
+
 
 # 课程类别
 class CourseType(models.Model):
 
+    uuid = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=20,unique=True)
+    # created_date = models.DateTimeField()
+
 
 # 课程类,不是实际上课时的。是可以复用的，
 class Courses(models.Model):
 
-    course_id = models.CharField(max_length=10,unique=True)
+    uuid = models.UUIDField()
+    course_code = models.CharField(max_length=10,unique=True)
     coursetype = models.ForeignKey(CourseType,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    # created_date = models.DateTimeField()
+
 
 class Terms(models.Model):
-    term_id = models.CharField(max_length=15,unique=True)
+    uuid = models.UUIDField()
+    term_string = models.CharField(max_length=20,unique=True)
     begin_date = models.DateField()
     end_date = models.DateField()
+    status = models.CharField(max_length=10)
+    # created_date = models.DateTimeField()
+
 
 class Classes(models.Model):
-    class_id = models.IntegerField()
-    course = models.ForeignKey(Courses)
+    uuid = models.UUIDField()
+    coupirse = models.ForeignKey(Courses,on_delete=models.CASCADE)
     class_begin = models.DateTimeField()
     class_end = models.DateTimeField()
-    term = models.ForeignKey(Terms)
+    term = models.ForeignKey(Terms,on_delete=models.CASCADE)
     capacity = models.IntegerField()
+    status = models.CharField(max_length=10)
+    # created_date = models.DateTimeField()
+
     # 课程和老师的关系是一对多
 # 原型类>>>-----------------------------------------------------------------
 
 # 关系类<<<-----------------------------------------------------------------
-# class ClassAndTeacher(models.Model):
-# class ClassAndStudent(models.Model):
+class ClassTeacher_rel(models.Model):
+    uuid = models.UUIDField()
+    teacher_id = models.ForeignKey(Teachers,on_delete=models.CASCADE)
+    classid = models.ForeignKey(Classes,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    # created_date = models.DateTimeField()
+
+class ClassStudent_rel(models.Model):
+    uuid = models.UUIDField()
+    student = models.ForeignKey(Students,on_delete=models.CASCADE)
+    classid = models.ForeignKey(Classes,on_delete=models.CASCADE)
+    status = models.CharField(max_length=10)
+    # created_date = models.DateTimeField()
+# 关系类>>>-----------------------------------------------------------------
